ed2330d97dc483a6cd60595e2f838d82fa065be9
Ahmed Atef <ahmedatifmahmoud@gmail.com>
2023-05-09 23:06:02 +0300

    getAffectedDiscs

--- othelloController.js
+++ othelloController.js
@@ -83,3 +83,80 @@ function drawDiscs() {
         }
     }
 }
+
+
+function getAffectedDiscs(id, row, column) {
+    var affectedDiscs = [];
+
+    //flip the right
+    var couldBeAffected = [];
+    var columnIterator = column;
+    while (columnIterator < 7) {
+        columnIterator += 1;
+        var valueAtSpot = discs[row][columnIterator];
+        if (valueAtSpot == 0 || valueAtSpot == id) {
+            if (valueAtSpot == id) {
+                affectedDiscs = affectedDiscs.concat(couldBeAffected);
+            }
+            break;
+        }
+        else {
+            var discLocation = { row: row, column: columnIterator };
+            couldBeAffected.push(discLocation);
+        }
+    }
+
+    //flip the left
+    var couldBeAffected = [];
+    var columnIterator = column;
+    while (columnIterator > 0) {
+        columnIterator -= 1;
+        var valueAtSpot = discs[row][columnIterator];
+        if (valueAtSpot == 0 || valueAtSpot == id) {
+            if (valueAtSpot == id) {
+                affectedDiscs = affectedDiscs.concat(couldBeAffected);
+            }
+            break;
+        }
+        else {
+            var discLocation = { row: row, column: columnIterator };
+            couldBeAffected.push(discLocation);
+        }
+    }
+
+    //flip the up
+    var couldBeAffected = [];
+    var rowIterator = row;
+    while (rowIterator > 0) {
+        rowIterator -= 1;
+        var valueAtSpot = discs[rowIterator][column];
+        if (valueAtSpot == 0 || valueAtSpot == id) {
+            if (valueAtSpot == id) {
+                affectedDiscs = affectedDiscs.concat(couldBeAffected);
+            }
+            break;
+        }
+        else {
+            var discLocation = { row: rowIterator, column: column };
+            couldBeAffected.push(discLocation);
+        }
+    }
+
+    //flip the down
+    var couldBeAffected = [];
+    var rowIterator = row;
+    while (rowIterator < 7) {
+        rowIterator += 1;
+        var valueAtSpot = discs[rowIterator][column];
+        if (valueAtSpot == 0 || valueAtSpot == id) {
+            if (valueAtSpot == id) {
+                affectedDiscs = affectedDiscs.concat(couldBeAffected);
+            }
+            break;
+        }
+        else {
+            var discLocation = { row: rowIterator, column: column };
+            couldBeAffected.push(discLocation);
+        }
+    }
+}
